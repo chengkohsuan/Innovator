c65c9c8aa4787bcb340be72cf686cca14a1f7649
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-02-27 17:25:57 +0800

    02/27 edited

---
+++ news/js/news-post.js
@@ -0,0 +1,7 @@
+$(function(){
+	$(".layer-1 > li").hover(function(){
+		$(this).children('.layer-2').css('display','block');
+	},function(){
+		$(this).children('.layer-2').css('display','none');
+	});
+})
