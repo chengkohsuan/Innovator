84b64b6f07c9a4ca04f7081211d5ced86885bc35
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-03-11 17:45:52 +0800

    03/11 edited

--- products/arc/js/arc-450.js
+++ products/arc/js/arc-450.js
@@ -4,4 +4,32 @@ $(function(){
 	},function(){
 		$(this).children('.layer-2').css('display','none');
 	});
-})
+});
+
+var product_data={
+  pd_infos:[
+  {
+    name: "商品一",
+    text: "LKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZ",
+    features: ["AAA","BBB","CCC"],
+    specs: ["XXX","YYY","ZZZ"],
+  },
+  {
+    name: "商品二",
+    text: "LKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZ",
+    features: ["AAA","BBB","CCC"],
+    specs: ["XXX","YYY","ZZZ"],
+  },
+  {
+    name: "商品三",
+    text: "LKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZLKJHGFDSAPOIUYTREWQ><MNBVCXZ",
+    features: ["AAA","BBB","CCC"],
+    specs: ["XXX","YYY","ZZZ"],
+  }],
+};
+
+var vm_1=new Vue({
+  el: "#infos",
+  data: product_data
+});
+
