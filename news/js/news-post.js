41840d86d1a933ad917c956bdb7adf67d7eaa74c
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-03-20 11:11:55 +0800

    folder-news/error

--- news/js/news-post.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$(".layer-1 > li").hover(function(){
-		$(this).children('.layer-2').css('display','block');
-	},function(){
-		$(this).children('.layer-2').css('display','none');
-	});
-});
