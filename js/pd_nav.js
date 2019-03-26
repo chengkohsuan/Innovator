8bb91154614c2cdca313557dbb1dda3cd949eae4
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-03-26 14:15:22 +0800

    03/26 edited

--- js/pd_nav.js
+++ js/pd_nav.js
@@ -1,3 +1,8 @@
+// var device=navigator.userAgent;
+// if(device.match(/iPhone|Android/)){
+// 	windoe.location="../mobile/mindex.html"
+// };
+
 $(function(){
 	$(".layer-1 > li").hover(function(){
 		$(this).children('.layer-2').css('display','block');
