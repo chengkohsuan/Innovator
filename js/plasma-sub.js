8ba223332fadeeb337a20a67a2b4863a117278c9
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-03-29 17:29:10 +0800

    03/29 edited

--- js/plasma-sub.js
+++ js/plasma-sub.js
@@ -99,7 +99,7 @@ var data_axial3=[
     title: "Mettech Axial 3 高功率軸向送粉電漿噴槍",
     show_text: true,
     text: "Axial 3 電漿噴槍堅固、靈活，可應用各式各樣的塗層，專利設計的軸向送粉可有效將粉末或懸浮液均勻加熱，大幅增加堆積效益，以最經濟的方式獲得卓越的塗層。",
-    pics: ["../../image/plasma-axial3-01.jpg","../../image/plasma-axial3-02.jpg"],
+    pics: ["../../image/img-2-axial3-01.jpg","../../image/img-2-axial3-02.jpg","../../image/blank.jpg"],
     show_features: false,
     features: ["","",""],
     show_specs: false,
