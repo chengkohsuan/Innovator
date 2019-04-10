d2be378aecd5f1c5a9c13322c917cbfcbf0c90d1
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-04-10 13:53:01 +0800

    04/10 edited

--- js/flame-sub.js
+++ js/flame-sub.js
@@ -5,7 +5,7 @@ var data_guns=[
     title: "Saint-Gobain Top Jet ＆ Master Jet火焰噴槍",
     show_text: true,
     text: "Saint-Gobain的Top Jet 2與Master Jet 2多功能火焰噴塗槍，可噴塗金屬線材和Flexicord（陶瓷或陶金包芯線材），可以調控馬達轉速，以低熔點金屬提供高速送線，或是高熔點材料的低速送線。輕巧的火焰槍可在室內或是現場進行操作，可適用於大面積工作或精密噴塗。",
-    pics: ["../../image/img-2-flamegun-01.jpg","../../image/img-2-flamegun-02.jpg","../../image/blank.jpg"],
+    pics: ["../../image/img-2-flamegun-01.jpg","../../image/img-2-flamegun-02.jpg",],
     show_features: true,
     features: ["Top Jet 2具備高扭力氣動馬達，擁有五檔變速箱，可以讓使用者在定速度下以最高扭矩工作","Master Jet 2以電動馬達輸送線材降低耗氣量，搭配電動控制器可精確監控送線狀態"],
     show_specs: false,
--- js/parts-sub.js
+++ js/parts-sub.js
@@ -223,7 +223,7 @@ var data_suspension=[
     title: "YSZ Suspension",
     show_text: true,
     text: "SPS技術是EB-PVD技術的取代品，在柱狀的熱障塗層中，熱傳導低、壽命長。YSZ漿料可單獨作為熱障塗層使用，也可做為高溫熱障塗層的基底塗層使用。此漿料具有低粘度、使用壽命長和易分散的特點，符合EN/AS 9100的標準，可確保產品品質的穩定。",
-    pics: ["../../image/img-2-sus-01.jpg","../../image/img-2-sus-02.jpg","../../image/blank.jpg"],
+    pics: ["../../image/img-2-sus-01.jpg","../../image/img-2-sus-02.jpg",],
     show_susp: true,
     susps: [
       {
@@ -307,7 +307,7 @@ var data_suspension=[
     title: "GdZr Suspension （僅供實驗單位使用）",
     show_text: true,
     text: "專為SPS系統所設計，可以用來製備出柱狀熱障塗層。GdZr具有在高溫相穩定性高和導電性低的特點，適用於具有合適基底層的高溫TBC應用。YSZ塗層為TBC中最為重要的塗層之一，但由於這幾年燃氣輪機的發展快速，所產生的溫度已經超越YSZ塗層的極限，造成渦輪葉片的維修更加的頻繁，主要原因為YSZ的燒結溫度在1200oC，使YSZ的相結構趨向不穩定，而且因為經過燒結，其熱傳導也跟著提高。而另外一個會使YSZ塗層崩裂的原因為空氣汙染，大氣中含有相當多的CMAS顆粒（鈣鎂鋁矽酸鹽類），這些顆粒會附著在TBC柱狀結構的裂縫之中，經過劇烈的熱漲冷縮，導致TBC塗層與鍵結層（bond-coat）的崩裂。在氧化鋯中添加Gd和La已在工業中實行，Gd2Zr2O7在高溫狀態下具有優異的穩定性、低熱傳導、適度的膨脹係數以及抵抗CMAS顆粒的侵蝕能力。",
-    pics: ["../../image/img-2-sus-03.jpg","../../image/img-2-sus-04.jpg","../../image/blank.jpg"],
+    pics: ["../../image/img-2-sus-03.jpg","../../image/img-2-sus-04.jpg",],
     show_susp: true,
     susps: [
       {
@@ -351,7 +351,7 @@ var data_suspension=[
     title: "氧化鋁Al2O3 suspension",
     show_text: true,
     text: "可取代現有的APS技術，可以得到更高的密度、更低的表面粗糙度與更出色的硬度。此漿料的儲存時間長。並且容易重新分散。",
-    pics: ["../../image/img-2-sus-05.jpg","../../image/img-2-sus-06.jpg","../../image/blank.jpg"],
+    pics: ["../../image/img-2-sus-05.jpg","../../image/img-2-sus-06.jpg",],
     show_susp: true,
     susps: [
       {
@@ -419,7 +419,7 @@ var data_suspension=[
     title: "氧化釔Y2O3 Suspension",
     show_text: true,
     text: "主要應用於半導體的蝕刻腔體，由於製備出來的塗層密度更高，抵抗離子蝕刻的效果更好。使用此漿料所製備出來的塗層孔隙率低於0.5%，可以取代現有的APS技術。",
-    pics: ["../../image/img-2-sus-07.jpg","../../image/img-2-sus-08.jpg","../../image/blank.jpg"],
+    pics: ["../../image/img-2-sus-07.jpg","../../image/img-2-sus-08.jpg",],
     show_susp: true,
     susps: [
       {
