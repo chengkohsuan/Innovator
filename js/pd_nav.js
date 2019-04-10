006403eb54d78475874bee8579029fd75a711460
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-04-10 22:18:32 +0800

    04/10 edited

--- js/pd_nav.js
+++ js/pd_nav.js
@@ -1,5 +1,4 @@
 
-
 $(function(){
 	$(".layer-1 > li").hover(function(){
 		$(this).children('.layer-2').css('display','block');
@@ -7,9 +6,16 @@ $(function(){
 		$(this).children('.layer-2').css('display','none');
 	});
 });
-
+// // 選單收合
 $(function(){
-    $("#nav-toggle").on("click",function(){
-        $("#pd-nav").css('height','auto');
-    })
+    $("#pd-nav").hide();
+    $("#nav-toggle").click(function(){
+        $("#pd-nav").slideToggle();
+        return false;
+    });
+    $(".layer-2").hide();
+    $(".layer-1 > li").click(function(){
+    	$(this).children('.layer-2').slideToggle();
+    	return false;
+    });
 });
