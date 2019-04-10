9f8616bcfc5a51bf264370b4b77785efc56ae2f9
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-04-10 17:32:45 +0800

    04/10 edited

--- js/pd_nav.js
+++ js/pd_nav.js
@@ -1,7 +1,4 @@
-// var device=navigator.userAgent;
-// if(device.match(/iPhone|Android/)){
-// 	windoe.location="../mobile/mindex.html"
-// };
+
 
 $(function(){
 	$(".layer-1 > li").hover(function(){
@@ -10,15 +7,18 @@ $(function(){
 		$(this).children('.layer-2').css('display','none');
 	});
 	
-});
+// });
+
+// $(function(){
+// 	$("#nav-toggle").click(function(){
+// 		$("#pd_nav > .layer-1").slideToggle();
+// 	});
+
+// });
+
 
 $(function(){
-	$("#pd-nav > .button").click(function(){
-		$(this).children('.layer-1').css('display','block');
-	},function(){
-		$(this).children('.layer-1').css('display','none');
+	$("#nav-toggle").click(function(){
+		$("#pd_nav > .layer-1").slideToggle("slow");
 	});
 });
-//失敗
-
-
