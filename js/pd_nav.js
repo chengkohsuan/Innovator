c4f475cac9d99818a9499b0bee08bf2551bfee9b
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-04-10 17:47:09 +0800

    04/10 edited

--- js/pd_nav.js
+++ js/pd_nav.js
@@ -6,19 +6,4 @@ $(function(){
 	},function(){
 		$(this).children('.layer-2').css('display','none');
 	});
-	
-// });
-
-// $(function(){
-// 	$("#nav-toggle").click(function(){
-// 		$("#pd_nav > .layer-1").slideToggle();
-// 	});
-
-// });
-
-
-$(function(){
-	$("#nav-toggle").click(function(){
-		$("#pd_nav > .layer-1").slideToggle("slow");
-	});
 });
