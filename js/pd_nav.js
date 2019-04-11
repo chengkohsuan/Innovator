5f56284c5e78265cf30a505e1bcf9658ad299132
chengkohsuan <44027693+chengkohsuan@users.noreply.github.com>
2019-04-11 09:01:03 +0800

    04/11 edited

--- js/pd_nav.js
+++ js/pd_nav.js
@@ -7,15 +7,15 @@ $(function(){
 	});
 });
  // 選單收合
-$(function(){
-    $("#pd-nav").hide();
-    $("#nav-toggle").click(function(){
-        $("#pd-nav").slideToggle();
-        return false;
-    });
-    $(".layer-2").hide();
-    $(".layer-1 > li").click(function(){
-    	$(this).children('.layer-2').slideToggle();
-    	return false;
-    });
-});
+// $(function(){
+//     $("#pd-nav").hide();
+//     $("#nav-toggle").click(function(){
+//         $("#pd-nav").slideToggle();
+//         return false;
+//     });
+//     $(".layer-2").hide();
+//     $(".layer-1 > li").click(function(){
+//     	$(this).children('.layer-2').slideToggle();
+//     	return false;
+//     });
+// });
